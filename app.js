f4cfcca8bb2ed3b19360b2bd991a16e862fd67c2
kaikaci12 <somewonesomewone@gmail.com>
2024-03-31 15:01:02 +0400

    fix input error handling issues

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const dateInputs = document.querySelectorAll(".date");
 const cvcInput = document.querySelector(".cvc-input");
 const confirmBtn = document.querySelector(".confirm");
 const errorSpans = document.querySelectorAll(".error-span");
+const numberError = document.getElementById("number-error");
+const cardNumberInput = document.querySelector("#card-num-input");
 let thankYouWindow = false;
+
 confirmBtn.addEventListener("click", () => {
   let isError = false;
   cardInputs.forEach((item, index) => {
     if (item.value === "") {
       item.classList.add("wrong-format");
       errorSpans[index].classList.add("active-span");
+      numberError.classList.remove("active-span");
+
       isError = true;
     } else {
       item.classList.remove("wrong-format");
@@ -17,7 +22,33 @@ confirmBtn.addEventListener("click", () => {
     }
   });
 
+  if (isNaN(cardNumberInput.value)) {
+    thankYouWindow = false;
+    numberError.classList.add("active-span");
+  } else {
+    numberError.classList.remove("active-span");
+    thankYouWindow = true;
+  }
   if (!isError) {
     thankYouWindow = true;
+  }
+});
+
+cardInputs.forEach((item, index) => {
+  item.addEventListener("change", () => {
+    if (item.value === "") {
+      item.classList.add("wrong-format");
+      errorSpans[index].classList.add("active-span");
+      numberError.classList.remove("active-span");
+    } else {
+      item.classList.remove("wrong-format");
+      errorSpans[index].classList.remove("active-span");
+    }
+  });
+
+  if (isNaN(cardNumberInput.value)) {
+    numberError.classList.add("active-span");
+  } else {
+    numberError.classList.remove("active-span");
   }
 });
