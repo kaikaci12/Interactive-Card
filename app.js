4477bffde59781b1911b062a176f23cf74f9a557
kaikaci12 <somewonesomewone@gmail.com>
2024-03-31 16:08:54 +0400

    add confirm logic

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const cardInputs = document.querySelectorAll(".card-input");
-const dateInputs = document.querySelectorAll(".date");
-const cvcInput = document.querySelector(".cvc-input");
 const confirmBtn = document.querySelector(".confirm");
 const errorSpans = document.querySelectorAll(".error-span");
 const numberError = document.getElementById("number-error");
 const cardNumberInput = document.querySelector("#card-num-input");
+const newWindow = document.querySelector(".new-window");
+const forms = document.querySelector(".forms-container");
 let thankYouWindow = false;
 
 confirmBtn.addEventListener("click", () => {
   let isError = false;
+
   cardInputs.forEach((item, index) => {
     if (item.value === "") {
       item.classList.add("wrong-format");
       errorSpans[index].classList.add("active-span");
-      numberError.classList.remove("active-span");
-
       isError = true;
     } else {
       item.classList.remove("wrong-format");
@@ -23,14 +22,18 @@ confirmBtn.addEventListener("click", () => {
   });
 
   if (isNaN(cardNumberInput.value)) {
-    thankYouWindow = false;
+    isError = true;
     numberError.classList.add("active-span");
   } else {
     numberError.classList.remove("active-span");
-    thankYouWindow = true;
   }
+
   if (!isError) {
-    thankYouWindow = true;
+    forms.classList.add("form-active");
+    newWindow.classList.add("active-window");
+  } else {
+    forms.classList.remove("form-active");
+    newWindow.classList.remove("active-window");
   }
 });
 
